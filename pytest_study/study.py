8cd0e8ee411f4855f6fb7055daa62ac9e9fa7709
lf <2895074613@qq.com>
2022-06-24 21:26:57 +0800

    注释了一些代码

--- pytest_study/study.py
+++ pytest_study/study.py
@@ -1,12 +1,12 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from pytest_study.open import yamlload
-
-
-@pytest.mark.parametrize('udata', yamlload('./study.yaml'))
-def test_01(udata):
-    driver = webdriver.Chrome()
-    driver.get(udata['url'])
-    driver.find_element(udata['input']['key'],udata['input']['value']).send_keys(udata['txt'])
-    driver.find_element(udata['input']['key'],udata['input']['value']).click()
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from pytest_study.open import yamlload
+#
+#
+# @pytest.mark.parametrize('udata', yamlload('./study.yaml'))
+# def test_01(udata):
+#     driver = webdriver.Chrome()
+#     driver.get(udata['url'])
+#     driver.find_element(udata['input']['key'],udata['input']['value']).send_keys(udata['txt'])
+#     driver.find_element(udata['input']['key'],udata['input']['value']).click()
