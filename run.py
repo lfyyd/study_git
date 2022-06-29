2b22106b9bb921a941bf1262619ce76cba9a5454
lf <2895074613@qq.com>
2022-06-29 21:09:14 +0800

    cc

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import pytest
 import os
 
-if __name__ == '__main__':
-    pytest.main()
-
-
 # if __name__ == '__main__':
-#     pytest.main(['--alluredir', './byhy_po/test_file/allure-results'])
-#     os.system('allure generate ./byhy_po/test_file/allure-results -o ./byhy_po/test_file/reports')
+#     pytest.main()
+
+
+if __name__ == '__main__':
+    pytest.main(['--alluredir', './byhy_po/test_file/allure-results'])
+    os.system('allure generate ./byhy_po/test_file/allure-results -o ./byhy_po/test_file/reports')
 
 # if __name__ == '__main__':
 #     pytest.main(['--html=./byhy_po/test_file/byhy.html'])
